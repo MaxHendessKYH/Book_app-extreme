3b4fd8fb72c6357455d3d8e77ba890ba1040c6e8
maxmac <nekohigan@gmail.com>
2024-02-26 14:07:42 +0100

    Initial Commit

---
+++ Book_App_extreme/Book_App_extremeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_App_extremeApp.swift
+//  Book_App_extreme
+//
+//  Created by Max.Hendess on 2024-02-26.
+//
+
+import SwiftUI
+
+@main
+struct Book_App_extremeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book_App_extreme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Book_App_extreme
+//
+//  Created by Max.Hendess on 2024-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
