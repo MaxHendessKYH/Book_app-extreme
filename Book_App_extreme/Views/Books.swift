3202e15c841722a496bc7ce879f93ee1755dad99
AlhassanAli88 <ali.alhasan8181@gmail.com>
2024-03-01 10:31:00 +0100

    Fixed a listViewer

---
+++ Book_App_extreme/Views/Books.swift
@@ -0,0 +1,8 @@
+//
+//  Books.swift
+//  Book_App_extreme
+//
+//  Created by Ali Alhasan on 2024-02-29.
+//
+
+import Foundation
---
+++ Book_App_extreme/Views/ListViewer.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewer.swift
+//  Book_App_extreme
+//
+//  Created by Ali Alhasan on 2024-02-29.
+//
+
+import SwiftUI
+
+struct ListViewer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListViewer()
+}
