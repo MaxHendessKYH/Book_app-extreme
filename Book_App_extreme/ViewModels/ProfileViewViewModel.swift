a9b3d4be08495d212a6c4fe1207160843a435cf5
jhaard <jorgen.hard@gmail.com>
2024-03-07 13:03:12 +0100

    Added presentation-text save.

--- Book_App_extreme/ViewModels/ProfileViewViewModel.swift
+++ Book_App_extreme/ViewModels/ProfileViewViewModel.swift
@@ -1,20 +1,13 @@
-//
-//  File.swift
-//  Book_App_extreme
-//
-//  Created by Mehdi on 2024-02-27.
-//
-
 import Foundation
 import Firebase
 
 class ProfileViewViewModel: ObservableObject {
     
-    @Published var name: String = ""
     @Published var registrationDate: String?
+    @Published var presentation: String?
+    @Published var hasFetchedPresentation = false
     
     @Published var avatarString: String?
-    
     
     func getUserMail() -> String {
         if let user = Auth.auth().currentUser {
@@ -26,13 +19,68 @@ class ProfileViewViewModel: ObservableObject {
     
     func getUserName() -> String {
         if let user = Auth.auth().currentUser {
-            guard let nameHere = user.displayName else { return "no name" }
-            print("Användare: \(nameHere)")
+            guard let name = user.displayName else { return "User" }
               return name
           }
-        return "NOOOOO"
+        return "User"
       }
     
+    func getPresentation() -> String {
+        if let user = Auth.auth().currentUser {
+            let firestore = Firestore.firestore()
+            let presentationTextReference = firestore.collection("userPresentation").document(user.uid)
+            
+            presentationTextReference.getDocument { (document, error) in
+                if let document = document, document.exists {
+                        if let presentationText = document.data()?["text"] as? String {
+                            self.presentation = presentationText
+                        } else {
+                            print("No text found for user with ID: \(user.uid).")
+                        }
+                } else {
+                    print("Error fetching presentation: \(error?.localizedDescription ?? "Unknown error")")
+                }
+            }
+        }
+        
+        return presentation ?? "NOTHING"
+    }
+    
+    func changeDisplayName(newName: String) {
+        if let user = Auth.auth().currentUser {
+            let request = user.createProfileChangeRequest()
+            
+            if !newName.isEmpty {
+                request.displayName = newName
+                request.commitChanges { (error) in
+                    if let error = error {
+                        print("Error: \(error.localizedDescription)")
+                    } else {
+                        print("Name has successfully changed")
+                    }
+                }
+            } else {
+                print("Name cannot be empty")
+            }
+        } else {
+            print("User is not not logged in")
+        }
+    }
+    
+    func addPresentationText(presentationText: String) {
+        if let user = Auth.auth().currentUser {
+            let firestore = Firestore.firestore()
+            let presentationTextReference = firestore.collection("userPresentation").document(user.uid)
+            presentationTextReference.setData(["text": presentationText]) { (error) in
+                if let error = error {
+                    print("Error \(user.uid): \(error.localizedDescription)")
+                } else {
+                    print("Presentationtext added for user: \(user.uid)")
+                }
+            }
+        }
+    }
+
     func getUserRegistrationDate() -> String {
          if let user = Auth.auth().currentUser {
              let registrationDate = formatDateToString(from: user.metadata.creationDate!)
@@ -41,16 +89,25 @@ class ProfileViewViewModel: ObservableObject {
          } else {
              return "No date"
          }
-       
      }
-    func formatDateToString(from date: Date) -> String {
+    
+    func userLogOut() {
+        do {
+            try Auth.auth().signOut()
+            print("User has successfully logged out")
+        } catch let signOutError as NSError {
+            print("Error: \(signOutError.localizedDescription)")
+        }
+    }
+    
+   private func formatDateToString(from date: Date) -> String {
         let df = DateFormatter()
         df.dateStyle = .medium
         df.timeStyle = .medium
         return df.string(from: date)
     }
     
-    func changeAvatar() -> String {
+    private func changeAvatar() -> String {
         return avatarString ?? ""
     }
 }
--- Book_App_extreme/Views/ProfileView.swift
+++ Book_App_extreme/Views/ProfileView.swift
@@ -1,10 +1,3 @@
-//
-//  ProfileView.swift
-//  Book_App_extreme
-//
-//  Created by Mehdi on 2024-02-27.
-//
-
 import SwiftUI
 
 struct ProfileView: View {
@@ -17,6 +10,8 @@ struct ProfileView: View {
     @State var avatar: String = "person"
     
     @State var avatarColor: Color = Color.blue
+    @State var showEdit = false
+    @State var hasFetchedPresentation = false
     
     var body: some View {
         ScrollView {
@@ -24,101 +19,144 @@ struct ProfileView: View {
                 Text("Your Profile")
                     .font(.largeTitle)
                     .bold()
-                    Circle()
-                        .frame(width: 100, height: 100, alignment: .top)
-                        .foregroundStyle(avatarColor)
-                        .overlay(
-                            Image(systemName: getSytemImageString())
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50, height: 50)
-                                    .foregroundColor(.white)
-                        )
-                        .contextMenu {
-                             Button {
-                                 setSytemImageString(image: "person")
-                                 setAvatarColor(color: Color.blue)
-                             } label: {
-                                 Label("Person", systemImage: "person")
-                             }
-                            
-                            Button {
-                                setSytemImageString(image: "heart")
-                                setAvatarColor(color: Color.red)
-                            } label: {
-                                Label("Heart", systemImage: "heart")
-                            }
-                            
-                            Button {
-                                setSytemImageString(image: "star")
-                                setAvatarColor(color: Color.yellow)
-                            } label: {
-                                Label("Star", systemImage: "star")
-                            }
-                            
-                            Button {
-                                setSytemImageString(image: "smiley")
-                                setAvatarColor(color: Color.green)
-                            } label: {
-                                Label("Smiley", systemImage: "smiley")
-                            }
-                         }
-                    
-                    VStack {
-                        Text("Welcome,  \(viewModel.getUserName())")
-                            .bold()
-                            .font(.title2)
-                            .padding()
-                        Text("Mail: \t\(viewModel.getUserMail())")
-                            .bold()
-                            .font(.title3)
-                        Text("User since: \t \(viewModel.getUserRegistrationDate())")
-                            .bold()
-                            .font(.title3)
+                Circle()
+                    .frame(width: 100, height: 100, alignment: .top)
+                    .foregroundStyle(avatarColor)
+                    .overlay(
+                        Image(systemName: getSytemImageString())
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                    )
+                    .contextMenu {
+                        Button {
+                            setSytemImageString(image: "person")
+                            setAvatarColor(color: Color.blue)
+                        } label: {
+                            Label("Person", systemImage: "person")
+                        }
                         
+                        Button {
+                            setSytemImageString(image: "heart")
+                            setAvatarColor(color: Color.red)
+                        } label: {
+                            Label("Heart", systemImage: "heart")
+                        }
+                        
+                        Button {
+                            setSytemImageString(image: "star")
+                            setAvatarColor(color: Color.yellow)
+                        } label: {
+                            Label("Star", systemImage: "star")
+                        }
+                        
+                        Button {
+                            setSytemImageString(image: "smiley")
+                            setAvatarColor(color: Color.green)
+                        } label: {
+                            Label("Smiley", systemImage: "smiley")
+                        }
                     }
-                    .padding(20)
                 
-                Spacer()
+                VStack {
+                    Text("Welcome,  \(viewModel.getUserName())")
+                        .bold()
+                        .font(.title2)
+                        .padding()
+                    Text("Mail: \t\(viewModel.getUserMail())")
+                        .bold()
+                        .font(.title3)
+                    Text("User since: \t \(viewModel.getUserRegistrationDate())")
+                        .bold()
+                        .font(.title3)
+                    Text("About me".uppercased())
+                        .padding(5)
+                        .font(.title3)
+                    VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                        
+                        Text(fetchText(hasFetched: hasFetchedPresentation))
+                            .font(.system(size: 16))
+                            .bold()
+                            .italic()
+                            .foregroundStyle(.indigo)
+                        
+                        
+                        
+                    })
+                    
+                    
+                }
                 
-                Text("About Jorgen")
-                    .bold()
+                Button(showEdit ? "Hide edit" : "Show edit") {
+                    showEdit.toggle()
+                    hasFetchedPresentation = true
+                    
+                }
+                .padding(5)
                 
-                TextEditor(text: $presentationText)
-                              .border(.black, width: 1)
-                              .frame(height: 200)
-                     }
-            .padding(20)
-                        Button(action: {
-                            // Spara ändringar
-                            print("Button pressed!")
-                        }) {
-                            Text("Save Changes")
-                                .foregroundColor(.white)
-                                .bold()
-                                .padding(10)
-                                .background(.green)
-                                .cornerRadius(5)
+                if showEdit {
+                    
+                    Text("Name")
+                        .bold()
+                    TextField( "Your name: ", text: $name)
+                        .frame(width: 150, height: 40)
+                        .border(.gray, width: 1)
+                    
+                    Text("About You")
+                        .bold()
+                    
+                    TextEditor(text: $presentationText)
+                        .border(.gray, width: 1)
+                        .frame(height: 70)
+                        .onChange(of: presentationText, initial: false) { oldValue, newValue in
+                            
+                            presentationText = newValue
+                            
+                            
                         }
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            // Logga ut
-                           
-                        }) {
-                            Text("Log out")
-                                .foregroundColor(.white)
-                                .bold()
-                                .padding(10)
-                                .background(.red)
-                                .cornerRadius(5)
-                        }
-                        
-                    .padding(20)
+                }
                 
             }
+            .padding(20)
+            
+            if showEdit {
+                Button(action: {
+                    viewModel.changeDisplayName(newName: name)
+                    viewModel.addPresentationText(presentationText: presentationText)
+                    showEdit.toggle()
+                    print("Button pressed!")
+                }) {
+                    Text("Save Changes")
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(10)
+                        .background(.green)
+                        .cornerRadius(5)
+                }
+            }
+            Button(action: {
+                viewModel.userLogOut()
+            }) {
+                Text("Log out")
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding(10)
+                    .background(.red)
+                    .cornerRadius(5)
+            }
         }
+    }
+    
+    func fetchText(hasFetched: Bool) -> String {
+        if(!hasFetched) {
+            DispatchQueue.main.async {
+                presentationText = viewModel.getPresentation()
+            }
+        }
+        return presentationText
+    }
+    
     func setSytemImageString(image: String){
         avatar = image
     }
@@ -133,6 +171,13 @@ struct ProfileView: View {
     func getAvatarColor() -> Color {
         return avatarColor
     }
+    //    func setPresentation(presentation: String) {
+    //        presentationText = presentation
+    //    }
+    //
+    //    func getPresentation() -> String {
+    //        return presentationText
+    //    }
 }
 
 #Preview {
