7d5c4735d90f3408be3754afe8637ec3873a6bff
AlhassanAli88 <ali.alhasan8181@gmail.com>
2024-03-09 15:49:36 +0100

    Created UserId struct

---
+++ Book_App_extreme/Models/UserID.swift
@@ -0,0 +1,15 @@
+//
+//  UserID.swift
+//  Book_App_extreme
+//
+//  Created by Ali Alhasan on 2024-03-09.
+//
+
+import Foundation
+
+struct UserID{
+    
+    static let shared = UserID()
+    
+    var userID: String? 
+}
