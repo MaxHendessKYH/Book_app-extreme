05b51364db90b32369194d4335ac7f46b049f44c
Juhee <asmilejuliaz@gmail.com>
2024-03-11 23:17:50 +0100

    Refactoring 'AddBookShelfView'

--- Book_App_extreme/Views/AddBookShelfView.swift
+++ Book_App_extreme/Views/AddBookShelfView.swift
@@ -14,41 +14,39 @@ struct AddBookShelfView: View {
     
     @State var newTitel: String = ""
     var body: some View {
-        
         TextField("Type new shelf title", text: $newTitel)
             .padding()
+            .frame(height: 50)
+            .frame(maxWidth: 380)
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(10)
+            .padding(.top, 20)
         
-        Button("Add Shelf"){
-            
+        Button("Add"){
             addShelf()
         }
         .foregroundStyle(.white)
         .padding(13)
-        .background(.red)
-        .cornerRadius(30)
+        .bold()
+        .background(.blue)
+        .cornerRadius(10)
         .padding()
         .alert("Text is missing", isPresented: $isAlertpresented){
             Button("Ok", role: .cancel){}
-            
-            Spacer()
-            
         }
         Spacer()
-
+            .navigationBarTitle("Add Bookshelf")
     }
+    
     func addShelf() {
         
         if !newTitel.isEmpty{
             viewModel.addList(listTitel: newTitel)
-            
         }
-        
         else {
-            
             isAlertpresented = true
         }
     }
-    
 }
 /*
  #Preview {
