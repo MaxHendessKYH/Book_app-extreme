0568c8c71423af45ff200362e03b02ddfe0d2dc5
Juhee <asmilejuliaz@gmail.com>
2024-03-12 15:17:12 +0100

    Remove  "Hello It is me" on HomeView

--- Book_App_extreme/Views/HomeView.swift
+++ Book_App_extreme/Views/HomeView.swift
@@ -13,84 +13,35 @@ struct HomeView: View {
     
     var body: some View {
         NavigationView{
-           
-            
             VStack{
                 NavigationLink{
-                    
-                    AddBookShelfView(viewModel: viewModel) 
-                    
+                    AddBookShelfView(viewModel: viewModel)
                 }label :{
-                    
                     Image(systemName: "plus")
                 }
-                
-                
                 List{
-                    
-                    
-                    ForEach(viewModel.bookshelves!.indices, id: \.self){    index in
-                        
-                        
+                    ForEach(viewModel.bookshelves!.indices, id: \.self){index in
                         if let shelfData = viewModel.bookshelves?[index],
                            let titel = shelfData["titel"] as? String
                            //let books = shelfData["bookshelf"] as? [BookItem]
                            {
-                            
-                           
-                            
                             NavigationLink{
-                                
-                                
-                                
                                 BookListView(bookShelfIndex: index)
-
-                                
                             }label :{
-                                
-                                
                                 HStack{
                                     Image(systemName: "books.vertical") 
-                                    Text(titel) 
-                                    
+                                    Text(titel)
                                 }
                             }
-                            
-                            
-                           
-                            
-                            
                         }
-                        
-                        
                     }.onDelete(perform: { indexSet in
                         let shelfIndex = indexSet.first
-                        
                         viewModel.removeList(shelfIndex: shelfIndex ??  -1) 
                     })
-                    
-                    
                 }
-                
-                
-                
-                
-                
-                
-                
-                Text("Hello it is me")
-                    .onTapGesture {
-                        for shelf in viewModel.bookshelves!{
-                            
-                            let books = shelf["bookshelf"] as? [BookItem]
-                            let count = books!.count
-                            print(count)
-                        }
-                    }
             }
         }
     }
-    
 }
 
 #Preview {
